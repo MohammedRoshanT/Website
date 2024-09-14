4eeb2852f9bcf31dd9508008316fe2a179369139
Roshan <70361124+MohammedRoshanT@users.noreply.github.com>
2024-09-14 16:04:40 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/mr_roxhan"
+                  href="https://www.instagram.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
